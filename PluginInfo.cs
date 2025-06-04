6ae34259e28ea36bdb4127e65268fa5f37f0f9af
CreeperLogMat <creeperlogmat0205@gmail.com>
2025-06-04 18:29:50 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.creeperlogmat.gorillatag.menutemplate";
+        public const string Name = "Astral Cheat Menu";
+        public const string Description = "A menu made by CreeperLogMat";
+        public const string Version = "1.0.0";
+    }
+}
