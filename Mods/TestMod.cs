10fcce1b692630b66a59f60c9205ac6cddb85289
CreeperLogMat <creeperlogmat0205@gmail.com>
2025-06-04 18:30:33 -0400

    Add files via upload

---
+++ Mods/TestMod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AstralCheatMenu.Mods
+{
+    internal class TestMod
+    {
+        public static void TestSpeedMod ()
+        {
+            GorillaLocomotion.GTPlayer.Instance.maxJumpSpeed = 9f;
+            GorillaLocomotion.GTPlayer.Instance.jumpMultiplier = 9f;
+        }
+    }
+}
---
+++ Properties/AsseblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AstralCheatMenu.Properties
+{
+    internal class AsseblyInfo
+    {
+    }
+}
