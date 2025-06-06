bf2626f2478e45901f022879f5f62d9c799e4b06
CreeperLogMat <creeperlogmat0205@gmail.com>
2025-06-05 23:16:37 -0400

    Fucking hell

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -2,7 +2,7 @@
 {
     internal class PluginInfo
     {
-        public const string GUID = "org.creeperlogmat.gorillatag.ASM";
+        public const string GUID = "org.creeperlogmat.gorillatag.ASMProj";
         public const string Name = "Astral Cheat Menu";
         public const string Description = "A menu made by CreeperLogMat";
         public const string Version = "1.0.0";
